3b5c0a65938a67600015b09bec36e87fb0e0384b
JernieAlicias <jasmonda5@gmail.com>
2022-12-13 10:37:38 +0800

    tt
    tt

---
+++ MM_bot/mmbot (test).py
@@ -0,0 +1,168 @@
+import time, datetime, config, talib, pandas
+from binance.client import Client
+from mongodb import get_placeholder_buy, store_placeholder_buy, store_profit_history
+
+client = Client(config.api_key, config.api_secret, testnet=True)
+print("\nSuccessfully logged in", end="\n\n")
+
+SYMBOL        = 'ETHUSDT'
+ASSET         = 'USDT'
+TIMEFRAME     = '3m'
+QTY_TRADE     =  1
+
+# Prints the current date and time at the end of the display
+def print_datetime():
+    datetime1 = datetime.datetime.now()
+    now = str(datetime1.replace(microsecond=0))
+    print(f"Date & Time: {now}", end="\n\n")
+    return 
+
+# Gets our current position (number of crypto coins that we have)
+def get_position():
+    positions = Client.futures_account(self=client)['positions']
+    for p in positions:
+        if p['symbol'] == SYMBOL:
+           return float(p['positionAmt'])
+    return 0
+
+# Gets our current wallet balance for the compution of profit
+def get_wallet_balance():
+    balances = Client.futures_account(self=client)['assets']
+    for b in balances:
+        if b['asset'] == ASSET:
+           return float(b['walletBalance'])
+    return 0
+
+# Gets the latest (current) price of a Symbol based on the latest trade
+def get_last_price():
+    return float(Client.futures_recent_trades(self=client, symbol=SYMBOL)[-1]['price'])
+
+# Gets the mark price of a Symbol to be used for placeholder, calculation of profit, etc.
+def get_mark_price():
+    return float(Client.futures_mark_price(self=client, symbol=SYMBOL)['markPrice'])
+
+# Function that prints the current price, mark price, position, and other data on the display
+def print_data(id):
+    print(f"Current: {get_last_price()}  /  Mark Price: {get_mark_price()}  /  {id}: {get_placeholder_buy(id)}  /  Position: {position}")
+    print(f"Ema_3: {bars.ema_3.iloc[-1]}  /  Ema_6: {bars.ema_6.iloc[-1]}  /  LNRANG: {bars.LNRANG.iloc[-1]}")
+    return 
+
+# Gets the bars/klines in 1 minute data timeframe from Binance Futures then add the EMAs
+def get_bars():
+    datenow = datetime.datetime.now() - datetime.timedelta(days=2) # Used to indicate the date start of the bars
+    bars = client.futures_historical_klines(SYMBOL, interval=TIMEFRAME, 
+    start_str=datenow.strftime("%Y-%m-%d %H:%M:%S"))
+    bars = pandas.DataFrame(bars, columns=['time','open','high',
+    'low','close','vol','closetime','qav','trades','tbb','tbq','Nan'])
+    bars = bars.set_index(bars.columns[0])
+    bars["ema_3"]   = talib.EMA(bars.close, 3)
+    bars["ema_6"]   = talib.EMA(bars.close, 6)
+    bars["ema_13"]  = talib.EMA(bars.close, 13)
+    bars["ema_21"]  = talib.EMA(bars.close, 21)
+    bars["ema_35"]  = talib.EMA(bars.close, 35)
+    bars["SMA"]     = talib.SMA(bars.close, 500)
+    bars["SD"]      = talib.STDDEV(bars.close, 799, 1)
+    bars["LNRANG"]  = talib.LINEARREG_ANGLE(bars.close, 15)
+    return bars
+
+# Function that initiates the order to Buy in Binance Futures market
+def order_buy(id): 
+    m, x, y, z = get_mark_price(), bars.ema_6.iloc[-1], bars.ema_6.iloc[-2], bars.ema_3.iloc[-2]
+    client.futures_create_order(symbol=SYMBOL, side='BUY', type='MARKET', quantity=QTY_TRADE)   
+    store_placeholder_buy(id, datetime.datetime.now().strftime("%x"), datetime.datetime.now().strftime("%X"), m, x, y, z)      
+    print("Buying at Mark Price: ", m)
+    print(f'Symbol: {SYMBOL} / Side: BUY / Quantity: {QTY_TRADE} / Current Position: {get_position()}')
+    print(f"Sleeping for 5 seconds")
+    print_datetime()
+    time.sleep(5)
+
+# Function that initiates the order to Sell in Binance Futures market
+def order_sell():
+    m, x, y, z = get_mark_price(), bars.ema_6.iloc[-1], bars.ema_6.iloc[-2], bars.ema_3.iloc[-2]
+    client.futures_create_order(symbol=SYMBOL, side='SELL', type='MARKET', quantity=QTY_TRADE)     
+    profit = get_wallet_balance() - wallet_balance
+    store_profit_history(profit, datetime.datetime.now().strftime("%x"), datetime.datetime.now().strftime("%X"), m, x, y, z) 
+    print("Profit gain: ", profit)
+    print(f'Symbol: {SYMBOL} / Side: SELL / Quantity: {QTY_TRADE} / Current Position: {get_position()}')
+    print(f"Sleeping for 5 seconds")
+    print_datetime()
+    time.sleep(5)
+
+# Function for the buy and sell logic of mm_bot
+def buy_sell_logic(place, id, num):
+    print_data(id)
+
+    if position < num*QTY_TRADE:   # BUYING PHASE
+
+        if (get_mark_price() > (bars.SMA.iloc[-1] + bars.SD.iloc[-1]*50/68)) or (2 > bars.LNRANG.iloc[-1] > -2):
+            pass
+
+        # The following are the conditions to satisify to get in the buying range
+        elif (float(bars.ema_3.iloc[-1])  <  float(bars.ema_13.iloc[-1]) and
+              float(bars.ema_6.iloc[-1])  <  float(bars.ema_13.iloc[-1]) and
+              float(bars.ema_13.iloc[-1]) <  float(bars.ema_21.iloc[-1]) and
+              float(bars.ema_21.iloc[-1]) <  float(bars.ema_35.iloc[-1])):
+
+            # This condition tells the bot to wait for the lowest possible market price before buying
+            if float(bars.ema_3.iloc[-1]) < float(bars.ema_6.iloc[-1]):             
+               pass
+
+            # Once this condition is satisfied, the bot initiates the buy order
+            elif (float(bars.ema_3.iloc[-2])  <  float(bars.ema_6.iloc[-2])  and 
+                  float(bars.ema_13.iloc[-2]) <  float(bars.ema_21.iloc[-2]) and 
+                  float(bars.ema_13.iloc[-1]) <  float(bars.ema_21.iloc[-1]) and 
+                  float(bars.ema_6.iloc[-1])  <= get_mark_price()):
+                  pass
+
+            else: pass
+
+        else: pass
+            
+    else: # position = num*QTY_TRADE:   # SELLING PHASE
+        
+        # The following are the conditions to satisify to get in the selling range
+        if (float(bars.ema_3.iloc[-1])  >  float(bars.ema_13.iloc[-1]) and
+            float(bars.ema_6.iloc[-1])  >  float(bars.ema_13.iloc[-1]) and 
+            float(bars.ema_13.iloc[-1]) >  float(bars.ema_21.iloc[-1]) and
+            float(bars.ema_21.iloc[-1]) >  float(bars.ema_35.iloc[-1])):
+
+            # This code tells the bot to wait for the highest possible market price before selling
+            if float(bars.ema_3.iloc[-1]) > float(bars.ema_6.iloc[-1]) and get_mark_price() > place:
+               pass
+
+            # Once this condition is satisfied, the bot initiates the sell order
+            elif (float(bars.ema_3.iloc[-2])  >  float(bars.ema_6.iloc[-2])  and
+                  float(bars.ema_13.iloc[-2]) >  float(bars.ema_21.iloc[-2]) and 
+                  float(bars.ema_13.iloc[-1]) >  float(bars.ema_21.iloc[-1]) and 
+                  float(bars.ema_6.iloc[-1])  >= get_mark_price() > place + 1.5):
+                  pass
+
+            else: pass
+
+        else: pass
+
+    print_datetime()
+    print(bars)
+
+
+while True: # To loop the following codes as fast as possible (about 1 second)
+    
+    bars = get_bars() 
+    position = get_position()              
+    wallet_balance = get_wallet_balance()  
+
+    # The following new conditions are used to enable the bot to have 5 place (placeholders) for the buy value.
+    # The purpose of these codes is to enable the bot to continue the buy & sell logic even if the mark price 
+    # goes down unexpectedly and wait for the price to go up again to sell the previous place.
+
+    if (position == 0  or 
+       (position == 1 *QTY_TRADE and get_mark_price() >= get_placeholder_buy(id="place1")-3)): 
+        buy_sell_logic(get_placeholder_buy(id="place1"), id="place1", num=1)
+
+    for i in range(3):
+        if ((position == (i+1)*QTY_TRADE and get_mark_price() <  get_placeholder_buy(id="place"+str(i+1))-3) or 
+            (position == (i+2)*QTY_TRADE and get_mark_price() >= get_placeholder_buy(id="place"+str(i+2))-3)):
+             buy_sell_logic(get_placeholder_buy(id="place"+str(i+2)), id="place"+str(i+2), num=i+2)
+             break
+
+    if (position == 5 *QTY_TRADE): buy_sell_logic(get_placeholder_buy(id="place5"), id="place5", num=5)
---
+++ MM_bot/mongodb.py
@@ -0,0 +1,52 @@
+import pymongo, datetime
+
+mongodb_user_url = "mongodb+srv://admin:1234@mycluster.dyybuan.mongodb.net/?retryWrites=true&w=majority"
+mongodb_client = pymongo.MongoClient(mongodb_user_url)
+
+mm_bot = mongodb_client["Database"]            # Name of our overall database
+datab  = mm_bot["Collection"]                  # Name of collection for holding "place" and "total profit"
+profits_history = mm_bot["Profits (History)"]  # Name of collection for all of the list of profits collected
+profits_daily   = mm_bot["Profits (Daily)"]    # Name of collection for the list of daily profits collected
+
+
+# Used to insert new data in a collection for the first time
+def data_inserter():
+    place = { "_id": "place5", "date" : "", "time": "", "bought at": 0, "ema6[-1]":0, "ema6[-2]":0, "ema3[-2]":0} 
+    datab.insert_one(place) 
+
+# Used to insert new data in a collection for the first time
+def data_inserter2():
+    place = { "_id": "Buy", "Buy again" : True} 
+    datab.insert_one(place) 
+
+def store_should_buy(boolean):
+    datab.update_one( {"_id":"Buy"}, {"$set":{ "Should Buy?" : boolean}})
+
+def get_should_buy():
+    x = datab.find_one({"_id":"Buy"})
+    return x["Should Buy?"]
+
+# Used to store the date, time, and "bought at" value in the placeholder for buy
+def store_placeholder_buy(id, date, time, place, ema6_1, ema6_2, ema3_2):    
+    datab.update_one( {"_id":id}, {"$set":{ "date" : date, "time" : time, 
+    "bought at": place, "ema6[-1]": ema6_1, "ema6[-2]": ema6_2, "ema3[-2]": ema3_2}})
+
+# Used to get the "bought at" value in the placeholder for buy
+def get_placeholder_buy(id):
+    x = datab.find_one({"_id":id})
+    return x["bought at"]
+
+# Used to record the date, time, and profit gained into the database then adding it to the total profit
+def store_profit_history(profit, date, time, markprice, ema6_1, ema6_2, ema3_2):
+    new_profit = {"_id" : profits_history.count_documents({}) + 1, "date" : date, "time": time, 
+    "profit": profit, "price": markprice, "ema6[-1]": ema6_1, "ema6[-2]": ema6_2, "ema3[-2]": ema3_2} 
+    profits_history.insert_one(new_profit) 
+    totalp = datab.find_one({"_id":"Profit"})["total profit"]
+    datab.update_one( {"_id":"Profit"}, {"$set":{ "total profit": totalp + profit }})
+
+# Used to add up all of the profits from the database history
+def get_sum_profits():
+    sum_profit = 0
+    for x in profits_history.find({},{ "_id": 0, "profit": 1 }):
+        sum_profit += float(x["profit"])
+    return sum_profit
